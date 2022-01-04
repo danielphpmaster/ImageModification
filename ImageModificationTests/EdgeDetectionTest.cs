998024a090bbc55aab377017ba53886d36720fff
g.cathy <cathy.gay@students.hevs.ch>
2022-01-04 23:29:17 +0100

    Add tests for filters and edge detection, add resources

--- ImageModificationTests/EdgeDetectionTest.cs
+++ ImageModificationTests/EdgeDetectionTest.cs
@@ -1,9 +1,98 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using ImageModification.BLL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System.Drawing;
 
 namespace ImageModificationTests
 {
+    /// <summary>
+    /// EdgeDetectionTest unit test class to test the methods of the EdgeDetection class
+    /// </summary>
     [TestClass]
-    class EdgeDetectionTest
+    public class EdgeDetectionTest
     {
+        /// <summary>
+        /// CompareBitmap to compare images
+        /// </summary>
+        private CompareBitmap comparatorBitmap = new CompareBitmap();
+
+        /// <summary>
+        /// EdgeDetection interface that is substituted 
+        /// </summary>
+        private IEdgeDetection edgeDetection = Substitute.For<IEdgeDetection>();
+
+        /// <summary>
+        /// EdgeDetection class
+        /// </summary>
+        private EdgeDetection edgeDetectionClass = new EdgeDetection();
+
+        /// <summary>
+        /// Bitmap source image
+        /// </summary>
+        private Bitmap sourceImage = Properties.Resources.penguins;
+
+        /// <summary>
+        /// Laplacian3x3 edge detection test
+        /// </summary>
+        [TestMethod]
+        public void Laplacian3x3Test()
+        {
+            Bitmap filteredImage = Properties.Resources.penguins_laplacian3x3;
+
+            edgeDetection.filter(sourceImage, edgeDetection.getFilterMatrix("Laplacian3x3")).Returns(filteredImage);
+
+            Bitmap resultImage = edgeDetectionClass.filter(sourceImage, edgeDetectionClass.getFilterMatrix("Laplacian3x3"));
+
+            comparatorBitmap.CompareBitmapPixels(edgeDetection.filter(sourceImage, edgeDetection.getFilterMatrix("Laplacian3x3")), resultImage);
+
+        }
+
+        /// <summary>
+        /// Sobel3x3Vertical edge detection test
+        /// </summary>
+        [TestMethod]
+        public void Sobel3x3VerticalTest()
+        {
+            Bitmap filteredImage = Properties.Resources.penguins_Sobel3x3Vertical;
+
+            edgeDetection.filter(sourceImage, edgeDetection.getFilterMatrix("Sobel3x3Vertical")).Returns(filteredImage);
+
+            Bitmap resultImage = edgeDetectionClass.filter(sourceImage, edgeDetectionClass.getFilterMatrix("Sobel3x3Vertical"));
+
+            comparatorBitmap.CompareBitmapPixels(edgeDetection.filter(sourceImage, edgeDetection.getFilterMatrix("Sobel3x3Vertical")), resultImage);
+
+        }
+
+        /// <summary>
+        /// Prewitt3x3Horizontal edge detection test
+        /// </summary>
+        [TestMethod]
+        public void Prewitt3x3HorizontalTest()
+        {
+            Bitmap filteredImage = Properties.Resources.penguins_Prewitt3x3Horizontal;
+
+            edgeDetection.filter(sourceImage, edgeDetection.getFilterMatrix("Prewitt3x3Horizontal")).Returns(filteredImage);
+
+            Bitmap resultImage = edgeDetectionClass.filter(sourceImage, edgeDetectionClass.getFilterMatrix("Prewitt3x3Horizontal"));
+
+            comparatorBitmap.CompareBitmapPixels(edgeDetection.filter(sourceImage, edgeDetection.getFilterMatrix("Prewitt3x3Horizontal")), resultImage);
+
+        }
+
+        /// <summary>
+        /// Prewitt3x3Horizontal edge detection test
+        /// </summary>
+        [TestMethod]
+        public void DefaultEdgeDetectionTest()
+        {
+            Bitmap filteredImage = Properties.Resources.penguins_laplacian3x3;
+
+            edgeDetection.filter(sourceImage, edgeDetection.getFilterMatrix("")).Returns(filteredImage);
+
+            Bitmap resultImage = edgeDetectionClass.filter(sourceImage, edgeDetectionClass.getFilterMatrix(""));
+
+            comparatorBitmap.CompareBitmapPixels(edgeDetection.filter(sourceImage, edgeDetection.getFilterMatrix("")), resultImage);
+
+        }
     }
 }
--- ImageModificationTests/FiltersTest.cs
+++ ImageModificationTests/FiltersTest.cs
@@ -1,13 +1,82 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using ImageModification.BLL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System.Drawing;
 
 namespace ImageModificationTests
 {
+    /// <summary>
+    /// FiltersTest unit test class to test the methods of the Filters class
+    /// </summary>
     [TestClass]
     public class FiltersTest
     {
+        /// <summary>
+        /// CompareBitmap to compare images
+        /// </summary>
+        private CompareBitmap comparatorBitmap = new CompareBitmap();
+
+        /// <summary>
+        /// Filters interface that is substituted 
+        /// </summary>
+        private IFilters filter = Substitute.For<IFilters>();
+
+        /// <summary>
+        /// Filters class
+        /// </summary>
+        private Filters filterClass = new Filters();
+
+        /// <summary>
+        /// Bitmap source image
+        /// </summary>
+        private Bitmap sourceImage = Properties.Resources.penguins;
+
+        /// <summary>
+        /// Miami filter test
+        /// </summary>
         [TestMethod]
-        public void TestMethod1()
+        public void MiamiFilterTest()
         {
+            Bitmap filteredImage = Properties.Resources.penguins_miamiFilter;
+            
+            filter.ApplyFilter(sourceImage, 1, 1, 10, 1).Returns(filteredImage);
+
+            Bitmap resultImage = filterClass.ApplyFilter(sourceImage, 1, 1, 10, 1);
+
+            comparatorBitmap.CompareBitmapPixels(filter.ApplyFilter(sourceImage, 1, 1, 10, 1), resultImage);
+
+        }
+
+        /// <summary>
+        /// Black and White filter test
+        /// </summary>
+        [TestMethod]
+        public void BlackAndWhiteFilterTest()
+        {
+            Bitmap filteredImage = Properties.Resources.penguins_blackWhiteFilter;
+
+            filter.BlackWhite(sourceImage).Returns(filteredImage);
+
+            Bitmap resultImage = filterClass.BlackWhite(sourceImage);
+
+            comparatorBitmap.CompareBitmapPixels(filter.BlackWhite(sourceImage), resultImage);
+
+        }
+
+        /// <summary>
+        /// Swap filter test
+        /// </summary>
+        [TestMethod]
+        public void SwapFilterTest()
+        {
+            Bitmap filteredImage = Properties.Resources.penguins_swapFilter;
+
+            filter.ApplyFilterSwap(sourceImage).Returns(filteredImage);
+
+            Bitmap resultImage = filterClass.ApplyFilterSwap(sourceImage);
+
+            comparatorBitmap.CompareBitmapPixels(filter.ApplyFilterSwap(sourceImage), resultImage);
+
         }
     }
 }
